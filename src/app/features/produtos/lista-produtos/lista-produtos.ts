bb6be4fea5546d7175d37909fb6108b8b3962c87
Maria Clara Goncalves <mariaclaraestu@gmail.com>
2026-08-06 11:40:03 -0300

    writable signal, computed signal

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Produto } from '../produto/produto';
 
 @Component({
@@ -8,21 +8,33 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  //writable signal -> (reativo) que permitr alterações(com set ou update)
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
-    { nome: 'Mouse', preco: 179 },
+    { nome: 'Mouse', preco: 150 },
     { nome: 'Fone de ouvido', preco: 80 },
   ]);
+
+  //computed signal -> observa outro sinal e se atualiza automaticamente
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
 
   exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
     // Aqui você pode atualizar o estado, abrir modal, etc.
   }
 
+  //update -> adiciona um item a writable signal
   adicionarProduto() {
-    this.produtos.update((listaAtual) => [
-      ...listaAtual, 
-      { nome: 'Teclado', preco: 250 }
-    ]);
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
   }
+
+  //set -> altera um item do writable signal
+  substituirProdutos() {
+    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
+  }
+
 }
