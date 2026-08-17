c6b4950038fa6383f0e94f86d85b02d458ab81fb
Maria Clara Goncalves <mariaclaraestu@gmail.com>
2026-08-17 09:29:05 -0300

    30 / 25 - Reactive Forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,28 @@
- import { Routes } from '@angular/router';
-  import { authGuard } from './core/auth.guard';
-  export const routes: Routes = [
-    {
-      path: '',
-      loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
-    },
-    {
-      path: 'produtos',
-      loadComponent: () =>
-        import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
-    },
-   {
-      path: 'carrinho',
-      canActivate: [authGuard], // esse mano é o mestre mandou
-      loadComponent: () => import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
-    },
-    {
-      path: '**',
-      redirectTo: '', // redireciona para a página inicial
-    },
-  ];
+import { Routes } from '@angular/router';
+import { authGuard } from './core/auth.guard';
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
+  },
+  {
+    path: 'carrinho',
+    canActivate: [authGuard], // esse mano é o mestre mandou
+    loadComponent: () => import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+  },
+
+  {
+    path: 'checkout',
+    loadComponent: () => import('./features/checkout/checkout/checkout').then((m) => m.Checkout),
+  },
+
+  {
+    path: '**',
+    redirectTo: '', // redireciona para a página inicial
+  },
+];
